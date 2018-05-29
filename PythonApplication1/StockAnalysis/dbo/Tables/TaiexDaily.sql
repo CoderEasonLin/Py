ce0507eda812f26d1098aed4aba2f2f93bf06c99
CoderEasonLin <lin07646@gmail.com>
2018-05-30 00:12:26 +0800

    add table

---
+++ PythonApplication1/StockAnalysis/dbo/Tables/TaiexDaily.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TaiexDaily]
+(
+	[Date] DATE NOT NULL PRIMARY KEY, 
+    [TradingShares] BIGINT NOT NULL, 
+    [TradingTurnover] BIGINT NOT NULL, 
+    [Transaction] INT NOT NULL, 
+    [Taiex] DECIMAL(9, 2) NOT NULL, 
+    [Diff] DECIMAL(9, 2) NOT NULL
+)
