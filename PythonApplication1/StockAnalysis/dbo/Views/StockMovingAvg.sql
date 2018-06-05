3f2c5fa4a8460e930002d73fb7905397c1b751e6
CoderEasonLin <lin07646@gmail.com>
2018-06-05 11:19:07 +0800

    Update

--- PythonApplication1/StockAnalysis/dbo/Views/StockMovingAvg.sql
+++ PythonApplication1/StockAnalysis/dbo/Views/StockMovingAvg.sql
@@ -1,9 +1,8 @@
 ﻿CREATE VIEW [dbo].[StockMovingAvg]
-	AS SELECT TOP 60 [Date],
+	AS SELECT [Date],
 		StockId,
 		ClosePrice,
 		ROUND(AVG(ClosePrice) OVER (PARTITION BY StockId ORDER BY Date ASC ROWS 59 PRECEDING), 2) AS MA60,
 		ROUND(AVG(ClosePrice) OVER (PARTITION BY StockId ORDER BY Date ASC ROWS 19 PRECEDING), 2) AS MA20,
 		ROUND(AVG(ClosePrice) OVER (PARTITION BY StockId ORDER BY Date ASC ROWS 4 PRECEDING), 2) AS MA5   
 from StockDaily
-order by Date DESC
